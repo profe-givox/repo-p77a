ac82606cb890538daf3dd7dce8b52cfe0a1281d1
perro-negro <gi.vega@itsur.edu.mx>
2017-09-18 08:37:11 -0500

    se agrego el recycler view a la actividad principal

--- MiAudioLibros/app/src/main/java/net/ivanvega/miaudiolibros/AdaptadorLibros.java
+++ MiAudioLibros/app/src/main/java/net/ivanvega/miaudiolibros/AdaptadorLibros.java
@@ -14,36 +14,56 @@ import java.util.Vector;
  * Created by alcohonsilver on 12/09/17.
  */
 
-public class AdaptadorLibros extends RecyclerView.Adapter<AdaptadorLibros.ViewHolder> {
+public class AdaptadorLibros extends
+        RecyclerView.Adapter<AdaptadorLibros.ViewHolder> {
+    private View.OnClickListener onClickListener;
 
 
     private LayoutInflater inflador; //Crea Layouts a partir del XML protected Vector<Libro> vectorLibros; //Vector con libros a visualizar private Context contexto;
     private Vector<Libro> vectorLibros;
     private  Context contexto;
 
-    public AdaptadorLibros(Context contexto, Vector<Libro> vectorLibros) { inflador = (LayoutInflater) contexto
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE); this.vectorLibros = vectorLibros;
-            this.contexto = contexto;
-        }
-        //Creamos nuestro ViewHolder, con los tipos de elementos a modificar
-        public static class ViewHolder extends RecyclerView.ViewHolder { public ImageView portada;
+    public void setOnItemClickListerner(View.OnClickListener onClickListener){
+        this.onClickListener = onClickListener;
+    }
 
-            public TextView titulo;
-            public ViewHolder(View itemView) {
-                super(itemView);
-                portada = (ImageView) itemView.findViewById(R.id.portada); portada.setScaleType(ImageView.ScaleType.CENTER_INSIDE); titulo = (TextView) itemView.findViewById(R.id.titulo);
-            } }
-        // Creamos el ViewHolder con las vista de un elemento sin personalizar
-        @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) { // Inflamos la vista desde el xml
-            View v = inflador.inflate(R.layout.elemento_selector, null); return new ViewHolder(v);
+    public AdaptadorLibros(Context contexto, Vector<Libro> vectorLibros)
+    {
+        inflador = (LayoutInflater) contexto
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.vectorLibros = vectorLibros;
+            this.contexto = contexto;
+    }
+    //Creamos nuestro ViewHolder, con los tipos de elementos a modificar
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView portada;
+        public TextView titulo;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            portada = (ImageView) itemView.findViewById(R.id.portada);
+            portada.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+            titulo = (TextView) itemView.findViewById(R.id.titulo);
         }
+    }
+    // Creamos el ViewHolder con las vista de un elemento sin personalizar
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) { // Inflamos la vista desde el xml
+        View v = inflador.inflate(R.layout.elemento_selector, null);
+        v.setOnClickListener(onClickListener);
+        return new ViewHolder(v);
+    }
         // Usando como base el ViewHolder y lo personalizamos
         @Override
-        public void onBindViewHolder(ViewHolder holder, int posicion) { Libro libro = vectorLibros.elementAt(posicion); holder.portada.setImageResource(libro.recursoImagen); holder.titulo.setText(libro.titulo);
-        }
-        // Indicamos el número de elementos de la lista
-        @Override public int getItemCount() {
-            return vectorLibros.size(); }
+    public void onBindViewHolder(ViewHolder holder, int posicion) {
+            Libro libro = vectorLibros.elementAt(posicion);
+            holder.portada.setImageResource(libro.recursoImagen);
+            holder.titulo.setText(libro.titulo);
+    }
+
+    // Indicamos el número de elementos de la lista
+    @Override public int getItemCount() {
+        return vectorLibros.size();
+    }
 
 }
--- MiAudioLibros/app/src/main/java/net/ivanvega/miaudiolibros/MainActivity.java
+++ MiAudioLibros/app/src/main/java/net/ivanvega/miaudiolibros/MainActivity.java
@@ -2,12 +2,45 @@ package net.ivanvega.miaudiolibros;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView r_v;
+    AdaptadorLibros adp ;
+    RecyclerView.LayoutManager lmrv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        r_v = (RecyclerView)findViewById(R.id.recycler_view);
+        InfoGlobal info = InfoGlobal.getInstance();
+        info.inicializa(this);
+        adp = new AdaptadorLibros(this,
+                info.getVectorLibros() );
+        adp.setOnItemClickListerner(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplication(),
+                String.valueOf( r_v.getChildAdapterPosition((View)view)),
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        r_v.setAdapter(adp);
+
+
+        lmrv = new GridLayoutManager(this,2);
+        r_v.setLayoutManager(lmrv);
+
+
+
+
     }
 }
