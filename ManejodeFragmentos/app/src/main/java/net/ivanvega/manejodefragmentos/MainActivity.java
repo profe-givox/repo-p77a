1cc23c817bfbbd4685821ba686a9bad8c4ea78e9
perro-negro <gi.vega@itsur.edu.mx>
2017-10-24 08:39:02 -0500

    Se agrego el manejo de fragment manager

--- ManejodeFragmentos/app/src/main/java/net/ivanvega/manejodefragmentos/MainActivity.java
+++ ManejodeFragmentos/app/src/main/java/net/ivanvega/manejodefragmentos/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        android.app.FragmentManager fm = getFragmentManager();
+
+        Fragment fra = fm.findFragmentById(R.id.contenedor_fragment);
+
+
+        if(fra==null){
+            Toast.makeText(this, "El fragmento no esta cargado", Toast.LENGTH_LONG).show();
+        }
+
     }
 
     public void btnHardcode_click (View v){
@@ -25,13 +37,22 @@ public class MainActivity extends AppCompatActivity {
     public void btnDinamico_click(View v){
 
        android.app.FragmentManager fm =  getFragmentManager();
-         android.app.FragmentTransaction ft =  fm.beginTransaction();
 
-        FragemntoDos frag = new FragemntoDos();
 
-        ft.add(R.id.contenedor_fragment,  frag);
 
-        ft.commit();
+        if (fm.findFragmentById(R.id.contenedor_fragment)==null) {
+
+
+            android.app.FragmentTransaction ft = fm.beginTransaction();
+
+            FragemntoDos frag = new FragemntoDos();
+
+            ft.add(R.id.contenedor_fragment, frag);
+
+            ft.commit();
+        }else{
+            Toast.makeText(this, "El fragmento ya esta cargado esta cargado", Toast.LENGTH_LONG).show();
+        }
 
     }
 
