2e7ec26e55bfe78c237e8e6674898404cbc5d083
perro-negro <gi.vega@itsur.edu.mx>
2018-02-08 08:37:09 -0600

    se corrigio el bug que no permiria mostrar la lista del contetn provider a concultar, porque el Linear Layout principal no tenia orientacion

--- ClienteCPUsuarios/app/src/main/java/net/ivanvega/clientecpusuarios/MainActivity.java
+++ ClienteCPUsuarios/app/src/main/java/net/ivanvega/clientecpusuarios/MainActivity.java
@@ -4,6 +4,7 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
@@ -28,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
     {
         Cursor c  = getContentResolver().query (Uri.parse(  ContentProviderContractUser.CONTENT_URI ),
                 null,null,null,null );
+
+        Log.d("MICP", String.valueOf( c.getCount()));
 
         SimpleCursorAdapter sca = new SimpleCursorAdapter
                 (this, android.R.layout.simple_list_item_2,
