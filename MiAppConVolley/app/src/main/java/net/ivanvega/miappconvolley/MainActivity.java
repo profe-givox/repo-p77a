9c84c05738aa249122fa13224749e45f93dd332e
perro-negro <gi.vega@itsur.edu.mx>
2018-02-23 09:02:20 -0600

    Se implemento volley con personalizado y con singleton

--- MiAppConVolley/app/src/main/java/net/ivanvega/miappconvolley/MainActivity.java
+++ MiAppConVolley/app/src/main/java/net/ivanvega/miappconvolley/MainActivity.java
@@ -3,17 +3,30 @@ package net.ivanvega.miappconvolley;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
     // Instantiate the RequestQueue.
-    RequestQueue queue = Volley.newRequestQueue(this);
+    RequestQueue mRequestQueue;
+
+    // Instantiate the cache
+    Cache cache ;
+
+    // Set up the network to use HttpURLConnection as the HTTP client.
+    Network network ;
+
     String url ="http://www.google.com";
 
     // Request a string response from the provided URL.
@@ -23,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
                 public void onResponse(String response) {
                     // Display the first 500 characters of the response string.
                     Log.d("SUPERVOLLEY","Response is: "+ response.substring(0,500));
+                    Toast.makeText(MainActivity.this, "Response is: "+ response.substring(0,500),
+                            Toast.LENGTH_LONG).show();
                 }
             }, new Response.ErrorListener() {
         @Override
@@ -36,6 +51,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        queue.add(stringRequest);
+        cache = new DiskBasedCache(this.getCacheDir(), 1024 * 1024); // 1MB cap
+
+        network = new BasicNetwork(new HurlStack());
+
+        // Instantiate the RequestQueue with the cache and network.
+        mRequestQueue = new RequestQueue(cache, network);
+
+        // Start the queue
+        mRequestQueue.start();
+
+// Add the request to the RequestQueue.
+        mRequestQueue.add(stringRequest);
+
+
+
+
     }
 }
